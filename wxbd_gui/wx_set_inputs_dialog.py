d961a3eaddaa45c838a337b1b9da074086c4c5e7
ryanGT <ryanwkrauss@gmail.com>
2024-10-23 12:29:11 -0400

    debugging 3 inputs, take 6

--- wxbd_gui/wx_set_inputs_dialog.py
+++ wxbd_gui/wx_set_inputs_dialog.py
@@ -116,8 +116,13 @@ class SetInputsDialog(wx.Dialog):
     def get_block_name_from_widget(self, widget):
         ind = widget.GetSelection()
         print("ind: %s" % ind)
-        name = widget.GetString(ind)
-        return name
+        try:
+            name = widget.GetString(ind)
+            return name
+        except:
+            print("bad index")
+            print("ind: %s" % ind)
+            return None
 
 
     def get_block_instance_from_widget(self, widget):
